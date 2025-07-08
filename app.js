3d1374785388f56e9daf43aa32b3281d181acd1f
Kai Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-08 11:55:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+alert('Boas vindas ao nosso jogo!');
+
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+mensagemDeErro = alert('Erro! Preencha todos os campos.');
+nomeDoUsuario = prompt('Insira o seu nome de usário: ');
+idadeDoUsuario = prompt('Insira a sua idade: ');
+
+if ()
+if (idadeDoUsuario => 18){
+    alert('Pode tirar a habilitação!');
+
+} else{
+    alert('Infelizmente você ainda não pode tirar a habilitação...');
+}
+
+
