6bf1d84dcf48c8363acc06e6e58d721a4bc1c9d5
Kai Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-11 08:57:48 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-alert('Boas vindas ao nosso jogo!');
+//DESAFIO 1
 
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-let mensagemDeErro = alert('Erro! Preencha todos os campos.');
-let nomeDoUsuario = prompt('Insira o seu nome de usário: ');
-let idadeDoUsuario = prompt('Insira a sua idade: ');
+let diaDoUsuario = prompt("Qual dia da semana é hoje?");
 
-if (idadeDoUsuario >= 18) {
-    alert('Pode tirar a habilitação!');
+if (diaDoUsuario == "sábado" || "domingo") {
+   alert("Bom final de semana!");
 
-} else {
-    alert('Infelizmente você ainda não pode tirar a habilitação...');
+} else{
+   alert("Boa semana!");
 }
 
+//DESAFIO 2
 
+let numeroUsuario = prompt("Digite um número aleatório");
+
+if(numeroUsuario >=0) {
+    alert(`O número ${numeroUsuario} é positivo`);
+} else{
+    alert(`O número ${numeroUsuario} é negativo`);
+}
+
+//DESAFIO 3
+
+let tentativaUsuario = prompt("Escolha uma quantidade de pontos:");
+
+if(tentativaUsuario >= 100) {
+    alert("Parabéns, você venceu!");
+} else{
+    alert("Tente novamente para ganhar!");
+}
+
+//DESAFIO 4
+
+let saldoDoUsuario = 500;
+let respostaDoUsuario = prompt("Deseja saber o seu saldo?");
+
+if (respostaDoUsuario === "sim") {
+    alert(`O seu saldo atual é de: ${saldoDoUsuario}`);
+} else{
+    alert("Certo! Então, até a próxima!");
+}
+
+//DESAFIO 5 
+
+let nomeDeUsuario = prompt("Insira o seu nome:");
+alert(`Boas-vindas, ${nomeDeUsuario}!`);
